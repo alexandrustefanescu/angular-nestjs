a78a942f910c276c339f261a0f08c07cdffcd6a6
Alexandru Stefanescu <stefanescualexandru1994@gmail.com>
2025-11-23 10:27:41 +0200

    chore: add rate limiting for the api globally short, medium and long

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { TodosModule } from './todos/todos.module';
 import { validate } from './config/env.validation';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -11,6 +12,23 @@ import { validate } from './config/env.validation';
       validate,
       envFilePath: ['.env.local', '.env'],
     }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
     DatabaseModule,
     TodosModule,
   ],
